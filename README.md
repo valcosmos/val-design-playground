825d00a24b2f0cb725a1ceaa4aaea9671411552e
valcosmos <westerngod@126.com>
2022-03-30 14:34:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-jvn7fm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jvn7fm)
---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Button } from 'val-design';
+import 'val-design/dist/index.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to val-design</p>
+        <p>
+          <Button btnType="primary" size="large">
+            Button
+          </Button>
+          <Button style={{ marginLeft: '10px' }} btnType="danger" size="large">
+            Button
+          </Button>
+          <Button style={{ marginLeft: '10px' }} btnType="default" size="large">
+            Button
+          </Button>
+          <Button style={{ marginLeft: '10px' }} btnType="primary" size="sm">
+            Button
+          </Button>
+          <Button style={{ marginLeft: '10px' }} btnType="danger" size="sm">
+            Button
+          </Button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
