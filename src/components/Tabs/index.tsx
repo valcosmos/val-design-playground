62263675516e82f9339bb4b75f7e5160cfad63ee
Cupid Valentine <westerngod@126.com>
2022-04-14 23:47:01 +0800

    update Tab component and finished

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
-import {} from 'val-design'
+import { Icon, Tabs } from 'val-design'
 
 const CustomTabs = () => {
-  return <div>CustomTabs</div>
+  return (
+    <>
+      <h3>Tabs Component</h3>
+      <div className="">
+        <h4>Default tab</h4>
+        <Tabs>
+          <Tabs.Item label={'label one'}>content one</Tabs.Item>
+          <Tabs.Item label={'label two'}>content two</Tabs.Item>
+          <Tabs.Item label={'label three'}>content three</Tabs.Item>
+          <Tabs.Item
+            label={
+              <>
+                <Icon icon={'cannabis'} />
+                {'  '}Icon tab
+              </>
+            }
+          >
+            Icon tab content
+          </Tabs.Item>
+          <Tabs.Item disabled label={'disabled tab'}></Tabs.Item>
+        </Tabs>
+        <hr className='mt-3 mb-3' />
+        <h4>Card tab</h4>
+        <Tabs type="card">
+          <Tabs.Item label={'label one'}>content one</Tabs.Item>
+          <Tabs.Item label={'label two'}>content two</Tabs.Item>
+          <Tabs.Item label={'label three'}>content three</Tabs.Item>
+          <Tabs.Item
+            label={
+              <>
+                <Icon icon={'cannabis'} />
+                {'  '}Icon tab
+              </>
+            }
+          >
+            Icon tab content
+          </Tabs.Item>
+          <Tabs.Item disabled label={'disabled tab'}></Tabs.Item>
+        </Tabs>
+      </div>
+    </>
+  )
 }
 
 export default CustomTabs
