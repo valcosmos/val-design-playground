0f5ea3139797baba6399c6f0be1f705cf4930684
Cupid Valentine <westerngod@126.com>
2022-04-14 12:30:45 +0800

    add alert and finished

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Alert } from 'val-design'
 
 const CustomAlert = () => {
-  return <div>CustomAlert</div>
+  return (
+    <>
+      <h3>Alert Component</h3>
+      <div className="content" style={{ flexDirection: 'column' }}>
+        <Alert
+          title={'Default alert'}
+          closable={false}
+          onClose={() => {
+            console.log('you can handle some actions here')
+          }}
+        />
+        <Alert title={'Danger alert'} type="danger" />
+        <Alert title={'Warning alert'} type="warning" />
+        <Alert title={'success alert'} type="success" />
+        <hr />
+        <Alert title="alert title" description="alert description" />
+      </div>
+    </>
+  )
 }
 
 export default CustomAlert
