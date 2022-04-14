c7a07ffeb0db536ea8a94fba55a70676e90bed88
Cupid Valentine <westerngod@126.com>
2022-04-14 23:46:04 +0800

    update Icon component and finished

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,6 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import React from 'react'
 import { Icon } from 'val-design'
-import styles from './index.module.scss'
 
 const CustomIcon = () => {
   const icons: IconProp[] = [
@@ -31,18 +30,30 @@ const CustomIcon = () => {
   return (
     <>
       <h3>Icon Component</h3>
-      <a
-        href="https://fontawesome.com/v6/icons?m=free&s=solid"
-        target={'_blank'}
-      >
-        https://fontawesome.com/v6/icons?m=free&s=solid
-      </a>
-      <div className={styles.icons + ' content'}>
-        {icons.map((item) => (
-          <div>
-            <Icon icon={item} />
-          </div>
-        ))}
+
+      <div className="content col">
+        <a
+          href="https://fontawesome.com/v6/icons?m=free&s=solid"
+          target={'_blank'}
+        >
+          ==> https://fontawesome.com/v6/icons?m=free&s=solid
+        </a>
+        <p className='mt-2'>
+          The icons of this component library are based on the{' '}
+          <strong>fontawesome</strong> icon library The secondary package of ,
+          the supported icons can be obtained according to the above link
+        </p>
+        <p>
+          本组件库的图标是基于<strong>fontawesome</strong>图标库
+          的二次封装，所支持的图标，可根据以上面链接获取
+        </p>
+        <div className="icon">
+          {icons.map((item, index) => (
+            <div key={index}>
+              <Icon icon={item} size="2x" />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
