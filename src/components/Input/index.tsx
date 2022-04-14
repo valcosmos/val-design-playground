8dcacc4c893af6df4a92ee7d2b0dae0254ef0c05
Cupid Valentine <westerngod@126.com>
2022-04-14 12:32:47 +0800

    add input and finished

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { Input } from 'val-design'
 
 const CustomInput = () => {
+  const [data, setData] = useState('change event')
   return (
-    <div>CustomInput</div>
+    <>
+      <h3>Input Component</h3>
+      <div className="content" style={{ flexDirection: 'column' }}>
+        <Input placeholder="large" size="lg" />
+        <Input placeholder="small" size="sm" />
+        <Input placeholder="disabled" disabled />
+        <Input placeholder="icon" icon={'cannabis'}></Input>
+        <Input placeholder="prepend" prepend={'https://'}></Input>
+        <Input placeholder="append" append={'.com'}></Input>
+        <hr />
+        <p>{data}</p>
+        <Input
+          value={data}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setData(e.target.value)
+          }}
+        />
+      </div>
+    </>
   )
 }
 
