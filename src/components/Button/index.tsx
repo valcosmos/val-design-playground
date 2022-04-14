3973e99004b93622cad528d549cb64d1c2da75f0
Cupid Valentine <westerngod@126.com>
2022-04-14 12:31:21 +0800

    add button and finished

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,21 +6,23 @@ const CustomButton = () => {
   return (
     <>
       <h3>Button Component</h3>
-      <Button type="primary" size="lg">
-        Button
-      </Button>
-      <Button style={{ marginLeft: '10px' }} type="danger" size="lg">
-        Button
-      </Button>
-      <Button style={{ marginLeft: '10px' }} type="default" size="lg">
-        Button
-      </Button>
-      <Button style={{ marginLeft: '10px' }} type="primary" size="sm">
-        Button
-      </Button>
-      <Button style={{ marginLeft: '10px' }} type="danger" size="sm">
-        Button
-      </Button>
+      <div className="content">
+        <Button type="primary" size="lg">
+          Button
+        </Button>
+        <Button style={{ marginLeft: '10px' }} type="danger" size="lg">
+          Button
+        </Button>
+        <Button style={{ marginLeft: '10px' }} type="default" size="lg">
+          Button
+        </Button>
+        <Button style={{ marginLeft: '10px' }} type="primary" size="sm">
+          Button
+        </Button>
+        <Button style={{ marginLeft: '10px' }} type="danger" size="sm">
+          Button
+        </Button>
+      </div>
     </>
   )
 }
