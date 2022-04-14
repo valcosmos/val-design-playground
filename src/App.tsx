91e5c4a6a1ef50ea926e3848abef3486748d1254
Cupid Valentine <westerngod@126.com>
2022-04-14 23:42:42 +0800

    fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,8 @@ import './App.scss'
 // import styles
 import 'val-design/dist/index.css'
 
-import {
-  BrowserRouter as Router,
-  Link,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-  useNavigate
-} from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
-import Button from '@/components/Button'
-import Alert from '@/components/Alert'
-import AutoComplete from '@/components/AutoComplete'
 import { Menu } from 'val-design'
 
 function App() {
@@ -42,23 +31,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={'https://api.valzt.cn/media/avatar_me.png'} alt="logo" />
-        <h1 style={{ marginTop: '10px' }}>Welcome to val-design</h1>
-        <div>--{Button.name.split('Custom').pop()?.toString()}</div>
-        <Menu defaultIndex={index} onSelect={handleMenuSelect}>
-          {menu.map((m, i) => (
-            <Menu.Item key={i}>{m}</Menu.Item>
-          ))}
-        </Menu>
+        <h1 className="mt-3 mb-3">Welcome to val-design</h1>
+
+        <div className="menu">
+          <Menu defaultIndex={index} onSelect={handleMenuSelect}>
+            {menu.map((m, i) => (
+              <Menu.Item key={i}>{m}</Menu.Item>
+            ))}
+          </Menu>
+        </div>
       </header>
-      <main
-        style={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
+      <main className="main">
         <Outlet />
       </main>
     </div>
