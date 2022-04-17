cd534340f9af740418bdf6b3121a9ec4d78e0f9a
Cupid Valentine <westerngod@126.com>
2022-04-17 22:42:10 +0800

    modified router

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,10 @@
-import { useState } from 'react'
-import './App.scss'
-// import styles
-import 'val-design/dist/index.css'
+import { useRoutes } from 'react-router-dom'
 
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-
-import { Menu } from 'val-design'
+import routes from '@/router'
 
 function App() {
-  const [index, setIndex] = useState<string>('0')
-
-  const nav = useNavigate()
-  const location = useLocation()
-
-  const menu: string[] = [
-    'Button',
-    'Alert',
-    'AutoComplete',
-    'Icon',
-    'Input',
-    'Tabs',
-    'Upload'
-  ]
-  console.log(location)
-  const handleMenuSelect = (index: string) => {
-    console.log(index)
-    nav(`/${menu[+index].toLocaleLowerCase()}`)
-    setIndex(index)
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'https://api.valzt.cn/media/avatar_me.png'} alt="logo" />
-        <h1 className="mt-3 mb-3">Welcome to val-design</h1>
-
-        <div className="menu">
-          <Menu defaultIndex={index} onSelect={handleMenuSelect}>
-            {menu.map((m, i) => (
-              <Menu.Item key={i}>{m}</Menu.Item>
-            ))}
-          </Menu>
-        </div>
-      </header>
-
-      <main className="main">
-        <Outlet />
-      </main>
-    </div>
-  )
+  const element = useRoutes(routes)
+  return <>{element}</>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,64 +1,24 @@
 import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 
-import {
-  BrowserRouter as Router,
-  Link,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate
-} from 'react-router-dom'
+import '@/styles/common.scss'
+import 'val-design/dist/index.css'
+
+import App from './App'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
 
-import Button from './components/Button'
-import Alert from './components/Alert'
-import AutoComplete from './components/AutoComplete'
-import Icon from '@/components/Icon'
-import Input from '@/components/Input'
-import Tabs from '@/components/Tabs'
-import Upload from '@/components/Upload'
-import { Transition } from 'val-design'
-
-// const Button = lazy(() => import('@/components/Button'))
-// const Alert = lazy(() => import('@/components/Alert'))
-// const AutoComplete = lazy(() => import('@/components/AutoComplete'))
-// const Icon = lazy(() => import('@/components/Icon'))
-// const Input = lazy(() => import('@/components/Input'))
-// const Tabs = lazy(() => import('@/components/Tabs'))
-// const Upload = lazy(() => import('@/components/Upload'))
-
-const routesArr = [
-  { k: 'button', v: Button },
-  { k: 'alert', v: Alert },
-  { k: 'autocomplete', v: AutoComplete },
-  { k: 'icon', v: Icon },
-  { k: 'input', v: Input },
-  { k: 'tabs', v: Tabs },
-  { k: 'upload', v: Upload }
-]
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <Router basename="/">
-    {/* <Suspense fallback={<div></div>}> */}
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="" element={<Navigate to={'/button'} />}></Route>
-        {routesArr.map((r) => (
-          <Route path={r.k} element={<r.v />} key={r.k} />
-        ))}
-      </Route>
-    </Routes>
-    {/* </Suspense> */}
+  <Router>
+    <App />
   </Router>,
+  // </React.StrictMode>,
+
   document.getElementById('root')
 )
