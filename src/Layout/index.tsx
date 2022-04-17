d86729c4ea6a8195c8a58c8f9d56511ac7424ae4
Cupid Valentine <westerngod@126.com>
2022-04-17 22:39:27 +0800

    add layout

---
+++ src/Layout/index.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react'
+
+import { useState } from 'react'
+
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+
+import { Menu } from 'val-design'
+
+import style from './layout.module.scss'
+
+const Layout = () => {
+  const [index, setIndex] = useState<string>('0')
+
+  const nav = useNavigate()
+  const location = useLocation()
+
+  const menu: string[] = [
+    'Button',
+    'Alert',
+    'AutoComplete',
+    'Icon',
+    'Input',
+    'Tabs',
+    'Upload'
+  ]
+  console.log(location)
+  const handleMenuSelect = (index: string) => {
+    console.log(index)
+    nav(`/${menu[+index].toLocaleLowerCase()}`)
+    setIndex(index)
+  }
+  return (
+    <div className={style.layout}>
+      <header className="header">
+        <img src={'https://api.valzt.cn/media/avatar_me.png'} alt="logo" />
+        <h1 className="mt-3 mb-3">Welcome to val-design</h1>
+
+        <div className="menu">
+          <Menu defaultIndex={index} onSelect={handleMenuSelect}>
+            {menu.map((m, i) => (
+              <Menu.Item key={i}>{m}</Menu.Item>
+            ))}
+          </Menu>
+        </div>
+      </header>
+
+      <main className="main">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default Layout
